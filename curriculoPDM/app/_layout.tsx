c8366a09e13f237a2c85b9595ef9b8640474792d
elcio <elcioferreira398@gmail.com>
2025-11-07 14:58:50 +0000

    implementendo o react-native-paper e redirencionado do index para pasta tabs

--- curriculoPDM/app/_layout.tsx
+++ curriculoPDM/app/_layout.tsx
@@ -1,5 +1,25 @@
-import { Stack } from "expo-router";
+import { Stack } from 'expo-router';
+import { MD3DarkTheme, PaperProvider } from 'react-native-paper';
+
+const customTheme = {
+  ...MD3DarkTheme,
+  colors: {
+    ...MD3DarkTheme.colors,
+    primary: '#6200ee',
+    secondary: '#03dac6',
+    background: '#121212',
+    surface: '#1e1e1e',
+    error: '#cf6679',
+  },
+};
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <PaperProvider theme={customTheme}>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </PaperProvider>
+  );
 }
